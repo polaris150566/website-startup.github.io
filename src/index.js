6eaf4e6f2f3c2dd3234e43e23ae73b45ab034c2c
CTY <polaris150566@mail.ustc.edu.cn>
2024-10-09 04:08:38 +0800

    git-rebuild2

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import load_pages_auto from './components/cover/pages.js';//把书本导入进来
+import { PageFlip } from 'page-flip';//书本的包
+import coverT from "components/cover/index.js";//导入cover里面的indexjs
+import throttle from 'utils/throttle.js';
+//import pages from 'components/cover/pages.js'
+//const cover = new coverT;
+coverT.$mount( {
+	target: document.body,
+	option: "append",
+} );
+
+// load_pages_auto( coverT, window.innerWidth );
+
+
+// document.addEventListener( 'keydown', ( event ) => {
+// 	if ( event.key === 'ArrowUp' || event.keyCode === 38 ){
+// 		event.preventDefault();
+// 		coverT.switchUp();
+// 	} else if( event.key === 'ArrowDown' ||event.keyCode === 40 ){
+// 		event.preventDefault();
+// 		coverT.switchDown();
+// 	}
+// } );
+// document.addEventListener('wheel',throttle((event)=>{
+// 	event.preventDefault();
+// 	if(event.deltaY<0){
+// 		coverT.switchUp();
+// 	}
+// 	else if(event.deltaY>0){
+// 		coverT.switchDown();
+// 	}
+// },500),{ passive: false })
+document.addEventListener( 'click', ( event ) => {
+	coverT.switchDown();
+} );
+
+
+import loading from './loading/loading.js';//开始的loading界面
+
+loading();
+
+// document.getElementById('cards_container').addEventListener('mouseenter',function(){
+
+// })
+import * as d3 from "d3";
+import Svg_process from"src/components/cover/MorphingSVG.js"
+const svg_process = new Svg_process
+svg_process.morphPathsSequential(d3.select("#path1"))

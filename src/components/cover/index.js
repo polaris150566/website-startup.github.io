54f476406f437b8d583ff08bc13ebde9ea31dab4
CTY <polaris150566@mail.ustc.edu.cn>
2024-10-09 18:34:08 +0800

    edition

--- src/components/cover/index.js
+++ src/components/cover/index.js
@@ -1,9 +1,18 @@
 import cover_and_frontpage from './index.efml';
+import cardT from './cards.eft'
+const s3array = [{text:'text1'},{text:'text2'},{text:'text3'}]
 
 class cover_and_frontpage_control extends cover_and_frontpage{//加设鼠标，滚轮，点击翻页效果
 	constructor(){
 		super( );
 		this.current_number = 1;
+		for(let i = 0;i<3;i++){
+			this.cardslist.push(new cardT({
+				$data:{
+					text:s3array[i].text,
+				}
+			}))
+		}
 	};
 }
 const Cover_and_frontpage  = new cover_and_frontpage_control();//实例化
--- src/components/navigation/template.js
+++ src/components/navigation/template.js
@@ -6,10 +6,10 @@ export default class extends template{
 	constructor( ...args ){
 		super( ...args );
 
-		links.unshift( new block( {
-			$data: {
-				text: ""
-			} }, [] ) );
+		links.unshift( new block( {$data: {text: ""} }, [] ) );
+		links.unshift( new block( {$data: {text: ""} }, [] ) );
+		links.unshift( new block( {$data: {text: ""} }, [] ) );
+		links.push( new block( {$data: {text: ""} }, [] ) );
 		// links.unshift( new logotemplate( { $data: { href: extern_baseURL } } ) );
 		this.topics = links;
 	}
